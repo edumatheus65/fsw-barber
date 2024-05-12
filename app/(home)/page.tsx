69e1a2a24d7ab92c725b0c7aee6a4701c2c5188a
edumatheus65 <ematheusneves13@gmail.com>
2024-05-12 17:39:08 -0300

    feat: add initial serach components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,5 +1,6 @@
 import Header from "../_components/header";
 import { formatDate } from "../helpers/dateHelpers";
+import Search from "./_components/search";
 
 const Home = () => {
   const formattedDate = formatDate(new Date());
@@ -11,6 +12,10 @@ const Home = () => {
       <div className="px-5 pt-5">
         <h2 className="text-xl font-semibold">Olá Miguel</h2>
         <p className="capitalize text-sm">{formattedDate}</p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
       </div>
     </div>
 
