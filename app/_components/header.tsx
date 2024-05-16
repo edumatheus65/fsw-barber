a4341d5fff1dc2203aa6f9c688c14067f589c74b
edumatheus65 <ematheusneves13@gmail.com>
2024-05-16 14:26:41 -0300

    fix: adjusting outine

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -69,7 +69,7 @@ const Header = () => {
                             </Button>
 
                             {data?.user && (
-                                <Button variant="outline" className='justify-start'>
+                                <Button variant="outline" className='justify-start' asChild>
                                     <Link href="/bookings">
                                         <CalendarIcon size={18} className="mr-2" />
                                         Agendamentos
