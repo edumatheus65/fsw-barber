6e6c8715517a3f59c18ebc60afe0d7479e328182
edumatheus65 <ematheusneves13@gmail.com>
2024-08-20 20:40:17 -0300

    feat: esure user can search to barbershops

--- app/(home)/_components/search.tsx
+++ app/(home)/_components/search.tsx
@@ -1,18 +1,67 @@
-"use client"
+"use client";
 
 import { Button } from "@/app/_components/ui/button";
 import { Input } from "@/app/_components/ui/input";
 import { SearchIcon } from "lucide-react";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/app/_components/ui/form";
+import { useRouter } from "next/navigation";
+
+const formSchema = z.object({
+  search: z
+    .string({
+      required_error: "Campo Obrigatório",
+    })
+    .trim()
+    .min(2, {
+      message: "O nome da barbearia deve ter pelo menos 2 caracteres",
+    }),
+});
 
 const Search = () => {
-    return (
-        <div className="flex items-center gap-2">
-            <Input placeholder="Busque por uma barbearia..."/>
-            <Button variant="default" size="icon">
-                <SearchIcon size={18} />
-            </Button>
-        </div>
-    );
-}
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    router.push(`barbershops?search=${data.search}`);
+  };
+
+  return (
+    <div className="flex items-center gap-2">
+      <Form {...form}>
+        <form
+          className="flex w-full gap-4"
+          onSubmit={form.handleSubmit(handleSubmit)}
+        >
+          <FormField
+            control={form.control}
+            name="search"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Busque por uma barbearia..." {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button variant="default">
+            <SearchIcon size={18} type="submit" />
+          </Button>
+        </form>
+      </Form>
+    </div>
+  );
+};
 
 export default Search;
