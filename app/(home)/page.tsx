3cce186fa0843f11891f2eb706e80691baa66e6f
edumatheus65 <ematheusneves13@gmail.com>
2024-08-20 20:35:27 -0300

    feat: esure user can search to barbershops

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -66,7 +66,9 @@ const Home = async () => {
 
         <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem barbershop={barbershop} />
+            </div>
           ))}
         </div>
       </div>
@@ -78,7 +80,9 @@ const Home = async () => {
 
         <div className="flex gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
           {barbershops.map((barbershop) => (
-            <BarbershopItem key={barbershop.id} barbershop={barbershop} />
+            <div key={barbershop.id} className="min-w-[167px] max-w-[167px]">
+              <BarbershopItem barbershop={barbershop} />
+            </div>
           ))}
         </div>
       </div>
