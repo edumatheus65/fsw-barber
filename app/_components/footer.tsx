9b04e5d44348f782342017fbc9da0ca125d1e391
edumatheus65 <ematheusneves13@gmail.com>
2024-08-21 15:09:13 -0300

    feat: ensure semantic tags are user on footer and header

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return (
-        <div className="w-full bg-secondary py-6 px-5 flex justify-center">
-            <p className="text-gray-400 text-xs font-bold opacity-75">@ 2024 copyright FSW Barber</p>
-        </div>
-    );
-}
+  return (
+    <footer className="w-full bg-secondary py-6 px-5 flex justify-center">
+      <p className="text-gray-400 text-xs font-bold opacity-75">
+        @ 2024 copyright FSW Barber
+      </p>
+    </footer>
+  );
+};
 
 export default Footer;
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -10,24 +10,26 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <Card>
-      <CardContent className="p5 py-8 justify-between flex flex-row">
-        <Link href="/">
-          <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
-        </Link>
-        <Sheet>
-          <SheetTrigger asChild>
-            <Button variant="outline" size="icon">
-              <MenuIcon size={16} />
-            </Button>
-          </SheetTrigger>
+    <header>
+      <Card>
+        <CardContent className="p5 py-8 justify-between flex flex-row">
+          <Link href="/">
+            <Image src="/logo.png" alt="FSW Barber" height={22} width={120} />
+          </Link>
+          <Sheet>
+            <SheetTrigger asChild>
+              <Button variant="outline" size="icon">
+                <MenuIcon size={16} />
+              </Button>
+            </SheetTrigger>
 
-          <SheetContent className="p-0">
-            <SideMenu />
-          </SheetContent>
-        </Sheet>
-      </CardContent>
-    </Card>
+            <SheetContent className="p-0">
+              <SideMenu />
+            </SheetContent>
+          </Sheet>
+        </CardContent>
+      </Card>
+    </header>
   );
 };
 
